48109de3b949a2bd6b7e47f323e948e182f792c4
Logan Serman <logan.serman@metova.com>
2015-03-24 15:59:18 -0500

    Allow optional domain in mandrill config

--- lib/metova/mandrill/railtie.rb
+++ lib/metova/mandrill/railtie.rb
@@ -3,10 +3,11 @@ module Metova
     class Railtie < ::Rails::Railtie
 
       initializer "metova.mandrill" do |app|
-        if ENV['MANDRILL_DOMAIN']
-
-          app.config.action_mailer.default_url_options = { host: (ENV['MANDRILL_DEFAULT_HOST'] || ENV['MANDRILL_DOMAIN']) }
+        if ENV['MANDRILL_USERNAME'] && ENV['MANDRILL_PASSWORD']
           app.config.action_mailer.delivery_method = :smtp
+          app.config.action_mailer.default_url_options = { host: ENV['MANDRILL_DEFAULT_HOST'] }
+          app.routes.default_url_options = app.config.action_mailer.default_url_options
+
           app.config.action_mailer.smtp_settings = {
             address: 'smtp.mandrillapp.com',
             port: 587,
@@ -14,10 +15,9 @@ module Metova
             authentication: 'login',
             user_name: ENV['MANDRILL_USERNAME'],
             password: ENV['MANDRILL_PASSWORD'],
-            domain: ENV['MANDRILL_DOMAIN']
           }
 
-          app.routes.default_url_options = app.config.action_mailer.default_url_options
+          app.config.action_mailer.smtp_settings.merge! domain: ENV['MANDRILL_DOMAIN'] if ENV['MANDRILL_DOMAIN']
 
         end
       end
