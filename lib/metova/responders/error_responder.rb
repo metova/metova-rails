ca00b0b5ab83e04adaa2d8d15fb17f5bf14eb0ec
Jacob Lewis <jacob.lewis@metova.com>
2015-08-13 12:07:28 -0500

    Created error responder

---
+++ lib/metova/responders/error_responder.rb
@@ -0,0 +1,12 @@
+module Metova
+  module Responders
+  	module ErrorResponder
+      def initialize(*)
+  	    super
+  	    if has_errors?
+  	      Rails.logger.error "Response failed with error(s): #{@resource.errors.full_messages.to_sentence}"
+  	    end
+  	  end
+  	end
+  end
+end
