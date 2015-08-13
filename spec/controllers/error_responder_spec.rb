22d9084ae8485ed1ca0f6f0cae8028c232a8c0c7
Jacob Lewis <jacob.lewis@metova.com>
2015-08-13 16:12:31 -0500

    verify that new entries to test.log are generated

--- spec/controllers/error_responder_spec.rb
+++ spec/controllers/error_responder_spec.rb
@@ -1,13 +1,26 @@
 describe Api::RegistrationsController do
   
-  before do
-  	@request.env['devise.mapping'] = Devise.mappings[:api_user]
-  end
+  before { @request.env['devise.mapping'] = Devise.mappings[:api_user] }
 
   it 'logs errors when requests cannot be processed' do
+  	initialMessageCount = getTestLogWordCount("error(s):")
     post :create, user: { email: 'test@testing.com', password: 'foo' }
     expect(response.status).to eq 422
     assert_response :unprocessable_entity
+    finalMessageCount = getTestLogWordCount("error(s):")
+    assert_operator initialMessageCount, :<, finalMessageCount
+  end
+
+private
+
+  def getTestLogWordCount(word)
+    test_log = File.open("./spec/dummy/log/test.log")
+    contents = test_log.read
+    wordCount = 0
+    contents.split(' ').each do |item|
+      wordCount +=1 if item == word
+    end
+    wordCount
   end
 
 end
