e0426b0c5f2822a3fb219549488649b07dfd7290
Jacob Lewis <jacob.lewis@metova.com>
2015-08-13 14:15:31 -0500

    re-labeled error responder test

--- spec/controllers/error_responder_spec.rb
+++ spec/controllers/error_responder_spec.rb
@@ -4,7 +4,7 @@ describe Api::RegistrationsController do
   	@request.env['devise.mapping'] = Devise.mappings[:api_user]
   end
 
-  it 'logs errors when requests fail with a code of 422' do
+  it 'logs errors when requests cannot be processed' do
     post :create, user: { email: 'test@testing.com', password: 'foo' }
     expect(response.status).to eq 422
     assert_response :unprocessable_entity
