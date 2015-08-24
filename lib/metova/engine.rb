d3fe808f8a201f26f3548c94af510b053f341064
Logan Serman <logan.serman@metova.com>
2015-08-23 21:46:58 -0500

    Start of Google provider

--- lib/metova/engine.rb
+++ lib/metova/engine.rb
@@ -6,14 +6,5 @@ module Metova
     require 'devise'
     require 'responders'
 
-    # run engine migrations when running bin/rake db:migrate from the main app
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths['db/migrate'].expanded.each do |expanded_path|
-          app.config.paths['db/migrate'] << expanded_path
-        end
-      end
-    end
-
   end
 end
--- lib/metova/oauth/google_provider.rb
+++ lib/metova/oauth/google_provider.rb
@@ -1,10 +1,27 @@
 module Metova
   module Oauth
     class GoogleProvider < GenericProvider
+      GOOGLE_API_URL = 'https://www.googleapis.com/plus/v1/people/me'
+      ME_URL = -> (token) { "#{GOOGLE_API_URL}/me?access_token=#{token}" }
 
       def authenticate
+        self.info = OmniAuth::AuthHash.new me
+        self.uid = info.id
+        self
       end
 
+      def provider
+        :facebook
+      end
+
+      private
+        def me
+          super do
+            client = OAuth2
+            token = OAuth2::AccessToken.new
+            JSON.parse URI.parse(ME_URL[access_token]).read
+          end
+        end
     end
   end
 end
